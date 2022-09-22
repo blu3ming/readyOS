a5be4c773e8dfb3756f0c28dd7733cbcd4889990
Josué Cruz <ljosuecm@gmail.com>
2022-09-22 12:51:20 -0500

    PEASS a PEAS

--- readyOS.sh
+++ readyOS.sh
@@ -52,7 +52,7 @@ echo -e "\n\033[1m[*] Instalando PEASS en /opt...\033[0m"
 echo -e "\n\033[1m[-] Nota: PEASS ya no aloja los binarios en el repo, sino en la seccion de releases. Se recomienda dirigirse a esta sección para descargar la última versión manualmente...\033[0m"
 sleep 10
 cd /opt
-mkdir PEASS
+mkdir PEAS
 cd /opt/PEAS
 wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20220918/linpeas.sh
 wget -q https://github.com/carlospolop/PEASS-ng/releases/download/20220918/winPEASany.exe
